86df09a2c839f77e7804261418783a0b1ca14771
NthPortal <mail.vanpelt@gmail.com>
2015-08-05 06:11:36 -0400

    Removed unused SubCommand class.

--- src/main/java/com/github/nthportal/uhc/SubCommand.java
+++
@@ -1,8 +0,0 @@
-package com.github.nthportal.uhc;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-
-public interface SubCommand {
-    boolean execute(CommandSender commandSender, Command command, String label, String[] args);
-}
