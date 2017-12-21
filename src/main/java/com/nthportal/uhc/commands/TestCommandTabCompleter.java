299ba044d1301981c71cf1500725ec3a849aba6c
NthPortal <nthportal@gmail.com>
2017-12-21 01:58:06 -0500

    Fix tab completion
    Add player tab completion for /uhc-test death.

--- src/main/java/com/nthportal/uhc/commands/TestCommandTabCompleter.java
+++ src/main/java/com/nthportal/uhc/commands/TestCommandTabCompleter.java
@@ -1,20 +1,33 @@
 package com.nthportal.uhc.commands;
 
 import com.nthportal.uhc.util.Util;
+import lombok.val;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TestCommandTabCompleter implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        if (args.length != 1) {
-            return Collections.emptyList();
-        } else {
-            return Util.filterAndSort(TestCommandExecutor.Events.all, args[0]);
+        switch (args.length) {
+            case 1:
+                return Util.filterAndSort(TestCommandExecutor.Events.all, args[0]);
+            case 2:
+                if (args[0].equalsIgnoreCase(TestCommandExecutor.Events.DEATH)) {
+                    val nameStart = args[1].toLowerCase();
+                    return Bukkit.getOnlinePlayers().stream()
+                            .map(Player::getName)
+                            .filter(name -> name.toLowerCase().startsWith(nameStart))
+                            .sorted()
+                            .collect(Collectors.toList());
+                }
         }
+        return Collections.emptyList();
     }
 }
