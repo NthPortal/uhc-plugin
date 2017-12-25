f2d510f017014911c430a7b01b7da9cd857ef82b
NthPortal <nthportal@gmail.com>
2017-12-24 22:34:53 -0500

    Rewrite /uhc-test command

--- src/main/java/com/nthportal/uhc/commands/TestCommandExecutor.java
+++ src/main/java/com/nthportal/uhc/commands/TestCommandExecutor.java
@@ -8,18 +8,26 @@ import com.nthportal.uhc.events.*;
 import com.nthportal.uhc.util.Util;
 import lombok.RequiredArgsConstructor;
 import lombok.Synchronized;
+import lombok.experimental.Accessors;
 import lombok.val;
+import org.apache.commons.lang.math.IntRange;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.util.logging.Level;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.nthportal.uhc.util.MessageUtil.*;
 
@@ -34,6 +42,102 @@ public class TestCommandExecutor implements CommandExecutor {
                     .setNameFormat("uhc-manager:config-tester")
                     .build());
     private volatile boolean busy = false;
+
+    private final Map<String, SubCommand> subCommands = Stream.of(
+            new NoArgsSubCommand(Events.COUNTDOWN_START) {
+                @Override
+                void execute(CommandSender sender) {
+                    doCountdownStart();
+                }
+            },
+            new NoArgsSubCommand(Events.FULL_COUNTDOWN) {
+                @Override
+                void execute(CommandSender sender) {
+                    doFullCountdown(sender);
+                }
+            },
+            new NoArgsSubCommand(Events.START) {
+                @Override
+                void execute(CommandSender sender) {
+                    doStart();
+                }
+            },
+            new NoArgsSubCommand(Events.FULL_START) {
+                @Override
+                void execute(CommandSender sender) {
+                    doFullStart(sender);
+                }
+            },
+            new NoArgsSubCommand(Events.STOP) {
+                @Override
+                void execute(CommandSender sender) {
+                    context.eventBus().post(new StopEvent());
+                }
+            },
+            new NoArgsSubCommand(Events.PAUSE) {
+                @Override
+                void execute(CommandSender sender) {
+                    context.eventBus().post(new PauseEvent(0));
+                }
+            },
+            new NoArgsSubCommand(Events.RESUME) {
+                @Override
+                void execute(CommandSender sender) {
+                    context.eventBus().post(new ResumeEvent(0));
+                }
+            },
+            new SubCommand(Events.COUNTDOWN_MARK, new IntRange(1)) {
+                @Override
+                boolean execute(CommandSender sender, String[] args) {
+                    OptionalInt opt = getNumericArg(sender, args[0]);
+                    opt.ifPresent(num -> context.eventBus().post(new CountdownMarkEvent(num)));
+                    return opt.isPresent();
+                }
+            },
+            new SubCommand(Events.EPISODE_CHANGE, new IntRange(1)) {
+                @Override
+                boolean execute(CommandSender sender, String[] args) {
+                    OptionalInt opt = getNumericArg(sender, args[0]);
+                    opt.ifPresent(num -> doEpisodeChange(num));
+                    return opt.isPresent();
+                }
+            },
+            new SubCommand(Events.MINUTE, new IntRange(1)) {
+                @Override
+                boolean execute(CommandSender sender, String[] args) {
+                    OptionalInt opt = getNumericArg(sender, args[0]);
+                    opt.ifPresent(num -> context.eventBus().post(new MinuteEvent(num)));
+                    return opt.isPresent();
+                }
+            },
+            new SubCommand(Events.DEATH, new IntRange(0, 1)) {
+                @Override
+                boolean execute(CommandSender sender, String[] args) {
+                    Optional<Player> opt =  (args.length == 1) ? getPlayerArg(sender, args[0]) : commandSenderAsPlayer(sender);
+                    opt.ifPresent(player -> doPlayerDeath(player));
+                    return opt.isPresent();
+                }
+            },
+            new SubCommand(Events.PVP_KILL, new IntRange(1, 2)) {
+                @Override
+                boolean execute(CommandSender sender, String[] args) {
+                    Optional<Player> killerOpt = (args.length == 2) ? getPlayerArg(sender, args[0]) : commandSenderAsPlayer(sender);
+                    Optional<Player> valid = killerOpt.flatMap((Player killer) -> {
+                        Optional<Player> corpseOpt = getPlayerArg(sender, args[args.length - 1])
+                                .filter((Player corpse) -> {
+                                    boolean same = killer.equals(corpse);
+                                    if (same) {
+                                        sendError(sender, "Killer and corpse cannot be the same player");
+                                    }
+                                    return !same;
+                                });
+                        corpseOpt.ifPresent(corpse -> doPlayerKill(killer, corpse));
+                        return corpseOpt;
+                    });
+                    return valid.isPresent();
+                }
+            }
+    ).collect(Collectors.toMap(sc -> sc.event, Function.identity()));
 
     @Synchronized
     private boolean submitTask(Runnable task) {
@@ -63,122 +167,29 @@ public class TestCommandExecutor implements CommandExecutor {
         // Check permission to test UHC while in progress
         if (sender.hasPermission(Permissions.UHC) || timer.state() == Timer.State.STOPPED) {
             val event = args[0].toLowerCase();
-            if (Events.all.contains(event)) {
-                return doTest(sender, event, Util.arrayTail(args));
-            } else {
+            val subCommand = subCommands.get(event);
+            if (subCommand == null) {
                 sendError(sender, "Invalid event: " + args[0]);
-                return false;
+            } else {
+                // Check arg count
+                val count = args.length - 1;
+                if (count < subCommand.argCount.getMinimumInteger()) {
+                    sendError(sender, "Insufficient arguments for event: " + event);
+                } else if (count > subCommand.argCount.getMaximumInteger()) {
+                    if (subCommand.argCount.getMaximumInteger() == 0) {
+                        sendError(sender, "Event does not take arguments: " + event);
+                    } else {
+                        sendError(sender, "Too many arguments for event: " + event);
+                    }
+                } else {
+                    return subCommand.execute(sender, Util.arrayTail(args));
+                }
             }
+            return false;
         } else {
             sendError(sender, "You don't have permission to run this command while a UHC is in progress");
             return true;
         }
-    }
-
-    private boolean doTest(CommandSender sender, String event, String[] args) {
-        if (args.length > 1) {
-            sendError(sender, "Too many arguments");
-            return false;
-        }
-
-        context.plugin().reloadConfig();
-        if (args.length == 1) {
-            return doTestHasArg(sender, event, args[0]);
-        } else {
-            return doTestNoArgs(sender, event);
-        }
-    }
-
-    private boolean doTestNoArgs(CommandSender sender, String event) {
-        switch (event) {
-            case Events.COUNTDOWN_START:
-                doCountdownStart();
-                break;
-            case Events.FULL_COUNTDOWN:
-                doFullCountdown(sender);
-                break;
-            case Events.START:
-                doStart();
-                break;
-            case Events.FULL_START:
-                doFullStart(sender);
-                break;
-            case Events.STOP:
-                context.eventBus().post(new StopEvent());
-                break;
-            case Events.PAUSE:
-                context.eventBus().post(new PauseEvent(0));
-                break;
-            case Events.RESUME:
-                context.eventBus().post(new ResumeEvent(0));
-                break;
-            default:
-                sendError(sender, "Event '" + event + "' requires argument");
-        }
-        return true;
-    }
-
-    private boolean doTestHasArg(CommandSender sender, String event, String arg) {
-        if (Events.acceptingNumericArg.contains(event)) {
-            try {
-                val number = Integer.parseInt(arg);
-                return doTestNumericArg(sender, event, number);
-            } catch (NumberFormatException ignored) {
-                sendError(sender, "Event '" + event + "' requires numeric argument");
-                return false;
-            }
-        } else if (Events.acceptingPlayerArg.contains(event)) {
-            val player = Bukkit.getPlayerExact(arg);
-            if (player == null) {
-                sendError(sender, "Player not found: " + arg);
-                return false;
-            } else {
-                return doTestPlayerArg(sender, event, player);
-            }
-        } else {
-            return doTestStringArg(sender, event, arg);
-        }
-    }
-
-    private boolean doTestNumericArg(CommandSender sender, String event, int number) {
-        if (number <= 0) {
-            sendError(sender, "Number argument must be positive");
-            return false;
-        } else if (number > Short.MAX_VALUE) { // Sanity check
-            sendError(sender, "Number argument too large");
-            return false;
-        }
-
-        switch (event) {
-            case Events.COUNTDOWN_MARK:
-                context.eventBus().post(new CountdownMarkEvent(number));
-                break;
-            case Events.EPISODE_CHANGE:
-                doEpisodeChange(number);
-                break;
-            case Events.MINUTE:
-                context.eventBus().post(new MinuteEvent(number));
-                break;
-            default:
-                throw Util.impossible();
-        }
-        return true;
-    }
-
-    private boolean doTestPlayerArg(CommandSender sender, String event, Player player) {
-        switch (event) {
-            case Events.DEATH:
-                context.eventBus().post(new PlayerDeathEvent(player));
-                break;
-            default:
-                throw Util.impossible();
-        }
-        return true;
-    }
-
-    private boolean doTestStringArg(CommandSender sender, String event, String arg) {
-        sendError(sender, "Event '" + event + "'does not take args");
-        return false;
     }
 
     private void doCountdownStart() {
@@ -254,6 +265,72 @@ public class TestCommandExecutor implements CommandExecutor {
         eventBus.post(new MinuteEvent(minute));
     }
 
+    private void doPlayerDeath(Player player) {
+        context.eventBus().post(new PlayerDeathEvent(player));
+    }
+
+    private void doPlayerKill(Player killer, Player corpse) {
+        context.eventBus().post(new PlayerDeathEvent(corpse));
+        context.eventBus().post(new PlayerKillEvent(killer, corpse));
+    }
+
+    @RequiredArgsConstructor
+    @Accessors(fluent = true)
+    private abstract class SubCommand {
+        private final String event;
+        private final IntRange argCount;
+
+        abstract boolean execute(CommandSender sender, String[] args);
+
+        OptionalInt getNumericArg(CommandSender sender, String arg) {
+            try {
+                val number = Integer.parseInt(arg);
+                if (number <= 0) {
+                    sendError(sender, "Number argument must be positive");
+                } else if (number > Short.MAX_VALUE) { // Sanity check
+                    sendError(sender, "Number argument too large");
+                } else {
+                    return OptionalInt.of(number);
+                }
+            } catch (NumberFormatException ignored) {
+                sendError(sender, "Event '" + event + "' requires numeric argument");
+            }
+            return OptionalInt.empty();
+        }
+
+        Optional<Player> getPlayerArg(CommandSender sender, String arg) {
+            val player = Bukkit.getPlayerExact(arg);
+            if (player == null) {
+                sendError(sender, "Player not found: " + arg);
+                return Optional.empty();
+            } else {
+                return Optional.of(player);
+            }
+        }
+
+        Optional<Player> commandSenderAsPlayer(CommandSender sender) {
+            if (sender instanceof Player) {
+                return Optional.of((Player) sender);
+            } else {
+                return Optional.empty();
+            }
+        }
+    }
+
+    private abstract class NoArgsSubCommand extends SubCommand {
+        NoArgsSubCommand(String event) {
+            super(event, new IntRange(0));
+        }
+
+        @Override
+        final boolean execute(CommandSender sender, String[] args) {
+            execute(sender);
+            return true;
+        }
+
+        abstract void execute(CommandSender sender);
+    }
+
     static class Events {
         static final String COUNTDOWN_START = "countdown-start";
         static final String COUNTDOWN_MARK = "countdown-mark";
@@ -266,6 +343,7 @@ public class TestCommandExecutor implements CommandExecutor {
         static final String EPISODE_CHANGE = "episode-change";
         static final String MINUTE = "minute";
         static final String DEATH = "death";
+        static final String PVP_KILL = "pvp-kill";
 
         static final Set<String> all = Util.unmodifiableSet(
                 COUNTDOWN_START,
@@ -278,15 +356,13 @@ public class TestCommandExecutor implements CommandExecutor {
                 RESUME,
                 EPISODE_CHANGE,
                 MINUTE,
-                DEATH
+                DEATH,
+                PVP_KILL
         );
 
-        static final Set<String> acceptingNumericArg = Util.unmodifiableSet(
-                COUNTDOWN_MARK,
-                EPISODE_CHANGE,
-                MINUTE
+        static final Set<String> acceptingPlayerArg = Util.unmodifiableSet(
+                DEATH,
+                PVP_KILL
         );
-
-        static final Set<String> acceptingPlayerArg = Util.unmodifiableSet(DEATH);
     }
 }
--- src/main/java/com/nthportal/uhc/commands/TestCommandTabCompleter.java
+++ src/main/java/com/nthportal/uhc/commands/TestCommandTabCompleter.java
@@ -1,7 +1,6 @@
 package com.nthportal.uhc.commands;
 
 import com.nthportal.uhc.util.Util;
-import lombok.val;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -10,6 +9,7 @@ import org.bukkit.entity.Player;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class TestCommandTabCompleter implements TabCompleter {
@@ -19,15 +19,30 @@ public class TestCommandTabCompleter implements TabCompleter {
             case 1:
                 return Util.filterAndSort(TestCommandExecutor.Events.all, args[0]);
             case 2:
-                if (args[0].equalsIgnoreCase(TestCommandExecutor.Events.DEATH)) {
-                    val nameStart = args[1].toLowerCase();
-                    return Bukkit.getOnlinePlayers().stream()
-                            .map(Player::getName)
-                            .filter(name -> name.toLowerCase().startsWith(nameStart))
-                            .sorted()
-                            .collect(Collectors.toList());
+                if (TestCommandExecutor.Events.acceptingPlayerArg.contains(args[0].toLowerCase())) {
+                    return getPlayerNames(args[1].toLowerCase());
                 }
+                break;
+            case 3:
+                if (args[0].equalsIgnoreCase(TestCommandExecutor.Events.PVP_KILL)) {
+                    return getPlayerNames(args[2].toLowerCase(),
+                            name -> !name.equalsIgnoreCase(args[1]));
+                }
+                break;
         }
         return Collections.emptyList();
+    }
+
+    private static List<String> getPlayerNames(String nameStart) {
+        return getPlayerNames(nameStart, name -> true);
+    }
+
+    private static List<String> getPlayerNames(String nameStart, Predicate<String> nameFilter) {
+        return Bukkit.getOnlinePlayers().stream()
+                .map(Player::getName)
+                .filter(name -> name.toLowerCase().startsWith(nameStart))
+                .filter(nameFilter)
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
